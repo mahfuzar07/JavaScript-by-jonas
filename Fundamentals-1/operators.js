df3c9d5ae81eefc3ca5cd4cd0c73eb964b8ae0ee
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:36:41 +0600

    Create operators.js

---
+++ Fundamentals-1/operators.js
@@ -0,0 +1,49 @@
+//JavaScript Arithmetic Operators
+//[ + addition ]
+let addition = 20 + 10;
+console.log(addition);
+//[ - subtraction ]
+//[ * multiplication ]
+//[ / division ]
+//[ % modulus ]
+//[ ** exponentiation ]
+//[ ++ increment ]
+let increment = 10;
+increment++;
+console.log(increment);
+//[ -- decrement ]
+
+//JavaScript Assignment Operators
+//[ =, +=, -=, *=, /=, %= ]
+let x = 10;
+x += 5;
+console.log(x); //  [(x=10+5)=15]
+
+//JavaScript Comparison Operators
+//[== equal to]
+a = 10;
+if (a == 10) {
+  console.log('This is Equal to 10');
+} else {
+  console.log('This is not Equal to 10');
+}
+//[=== equal value and equal type]
+//[ != not equal]
+//[ !== not equal value and equal type]
+//[ > greater than]
+//[ < less than]
+//[ > greater than or equal to ]
+//[ < less than or equal to]
+//[ ? ternary operator]
+let b = 9;
+b == 10
+  ? console.log('This is Equal to 10')
+  : console.log('This is not Equal to 10');
+
+//JavaScript Logical Operators
+// [&&	logical and]
+let m = true;
+let n = true;
+console.log(m && n);
+// [||	logical or]
+// [!	logical not]

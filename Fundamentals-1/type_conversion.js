e6b08aad5ee7018275b8ec478d935b7bd112bbb7
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:36:55 +0600

    Create type_conversion.js

---
+++ Fundamentals-1/type_conversion.js
@@ -0,0 +1,10 @@
+var fullName = 'Mahfuzar Rahman'; //string data type
+var age = 25; //number data type
+console.log(typeof fullName);
+console.log(typeof age);
+
+var age = String(25);
+console.log(typeof age); //convert number to string
+
+var fullName = Number('Mahfuzar Rahman');
+console.log(typeof fullName); //convert number to string

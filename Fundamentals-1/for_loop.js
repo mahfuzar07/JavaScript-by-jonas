eeb2e779b935311a3ec7b63e3704ad42ca892022
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:54:43 +0600

    Update for_loop.js

--- Fundamentals-1/for_loop.js
+++ Fundamentals-1/for_loop.js
@@ -1,3 +1,16 @@
 for (let x = 0; x <= 10; x++) {
   console.log(x);
 }
+
+//Find Prime Number or not
+const isPrime = (number) => {
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return 'This is not  Prime  Number';
+    } else {
+      return 'This Is Prime Number';
+    }
+  }
+};
+const result = isPrime(19);
+console.log(result);

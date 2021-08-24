9f9238c4ce9241e18b43aa1a947fc7d82bcb3887
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:36:31 +0600

    Create if_else.js

---
+++ Fundamentals-1/if_else.js
@@ -0,0 +1,7 @@
+const age = 17;
+if (age >= 18) {
+  console.log('sara can apply for driving licence');
+} else {
+  const yearLeft = 18 - age;
+  console.log(`sara is too young. wait another ${yearLeft} years`);
+}

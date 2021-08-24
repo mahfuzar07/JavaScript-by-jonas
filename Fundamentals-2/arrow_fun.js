a0909784e23bd2e354658d2aad0ea6fc1266f130
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-25 02:30:29 +0600

    Create arrow_fun.js

---
+++ Fundamentals-2/arrow_fun.js
@@ -0,0 +1,9 @@
+//Regular Function Declarations
+function vanilla_function() {
+  console.log('From vanilla Function');
+}
+vanilla_function();
+
+// Arrow Function
+let arrow_function = () => 'From ES6 Arrow Function';
+console.log(arrow_function());

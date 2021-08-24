84f3957018396b62c9056f40755183daec6afd95
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:54:46 +0600

    Update if_else.js

--- Fundamentals-1/if_else.js
+++ Fundamentals-1/if_else.js
@@ -5,16 +5,3 @@ if (age >= 18) {
   const yearLeft = 18 - age;
   console.log(`sara is too young. wait another ${yearLeft} years`);
 }
-
-//Find Prime Number or not
-const isPrime = (number) => {
-  for (let i = 2; i < number; i++) {
-    if (number % i == 0) {
-      return 'This is not  Prime  Number';
-    } else {
-      return 'This Is Prime Number';
-    }
-  }
-};
-const result = isPrime(19);
-console.log(result);

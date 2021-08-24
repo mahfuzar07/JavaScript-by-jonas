d87721c7358a243a806602cea68091fab3f6a2a8
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:36:27 +0600

    Create data_type.js

---
+++ Fundamentals-1/data_type.js
@@ -0,0 +1,7 @@
+var fullName = 'Mahfuzar Rahman'; //string data type
+var age = 25; //number data type
+var status = true; //boolean data type
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof status);

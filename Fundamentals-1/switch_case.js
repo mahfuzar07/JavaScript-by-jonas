5864a7fba565e9eb3f09431698fe3b9ff028f873
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-24 15:36:44 +0600

    Create switch_case.js

---
+++ Fundamentals-1/switch_case.js
@@ -0,0 +1,11 @@
+let day = 'monday';
+switch (day) {
+  case 'sunday':
+    console.log('plan course structure');
+    break;
+  case 'monday':
+    console.log('write code example');
+    break;
+  default:
+    console.log('Not a valid day');
+}

11ba24a39590b6b79b1aaae31e639b4c7af4d699
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-29 01:44:29 +0600

    Create spreed_operator.js

---
+++ data-structure-js/spreed_operator.js
@@ -0,0 +1,9 @@
+//Spreed Operator define [...]
+const arr = [7, 8, 9];
+const newArr = [1, 2, ...arr, 10];
+console.log(newArr);
+//
+const friends = ['shovon', 'mahfuzar', 'sohan'];
+const fbFriend = ['arif', 'shuvo', 'habib'];
+const addFriend = [...friends, ...fbFriend];
+console.log(addFriend);

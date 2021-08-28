1d3bbe285bdde8cf9348de3ed0f588697d268c01
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-29 01:44:33 +0600

    Create default_para.js

---
+++ function-closeLook/default_para.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 25000 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH124', 3);

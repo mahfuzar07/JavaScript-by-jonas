4bf5ef66d19d7ee2932e68f2489142cd81518f87
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2021-08-31 20:21:00 +0600

    Update Higher_order.js

--- function-closeLook/Higher_order.js
+++ function-closeLook/Higher_order.js
@@ -1,2 +1,25 @@
-//3. We can Pass Function as an Aguments
-//4. We can return function from another function
+//1. Function can Pass Function as an Arguments
+//2. Function can return function from another function
+var numbers = [1, 2, 3];
+
+var result = numbers.map(function (number) {
+  return number * 2;
+});
+console.log(result);
+
+///Another one Higher Oeder Function
+
+function add(a, b) {
+  return a + b;
+}
+function manipulation(a, b, func) {
+  var c = a + b;
+  var d = a - b;
+  function multiply() {
+    var m = func(a, b);
+    return c * d * m;
+  }
+  return multiply;
+}
+var multiply = manipulation(3, 4, add);
+console.log(multiply());
